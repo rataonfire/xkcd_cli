0056472b511810f702b1a0e32a362b529d1814a3
rat101 <rataonfire@proton.me>
2025-08-03 17:18:22 +0300

    feat: add possibility to use script as a terminal command

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+#!/usr/bin/env node
+import { program } from "commander";
 import { generatePassword } from "./passwordGen.ts";
-const password = generatePassword()
-console.log(password+" "+password.length)
+
+program
+  .name('xkcd')
+  .description('Generate XKCD-style passwords')
+  .version('1.0.0')
+  .action(() => {
+    const password = generatePassword();
+    console.log('Generated Password:', password);
+  });
+
+program.parse();
